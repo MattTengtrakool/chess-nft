5c148121f416fe8188a71e190c5eeb6c32b439ae
hileamlakb <ehmyitayew@college.harvard.edu>
2021-12-28 19:14:09 -0500

    Add piece map

--- jpeg_creator.py
+++ jpeg_creator.py
@@ -3,17 +3,25 @@
 import cv2
 import numpy as np
 
+
+PIECE_MAP = {
+    0: "nothing",
+    1: "pawn",
+    2: "knight",
+    3: "bishop",
+    4: "rook",
+    5: "queen",
+    6: "king"
+}
+
 n_blocks = 8
 block_dim = 75
 image_dim = block_dim * n_blocks
-
 
 dark_brown = (99, 136, 181)
 light_brown = (181, 217, 240)
 
 blank_image = np.zeros((image_dim, image_dim, 3), np.uint8)
-
-
 
 # find a smarter way to do this
 for row in range(n_blocks):
@@ -33,3 +41,6 @@ for row in range(n_blocks):
 cv2.imshow("board", blank_image)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+
+sample_input  =  [[0 for colr in range(n_blocks)] for row in range(n_blocks)]
