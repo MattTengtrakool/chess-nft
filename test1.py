911efd5ac7ad4c3f7b60ec4387d16ae60f2a77a3
hileamlakb <ehmyitayew@college.harvard.edu>
2022-01-01 15:14:49 -0500

    Add test file

---
+++ test1.py
@@ -0,0 +1,42 @@
+from jpeg_creator import *
+
+board = generate_board()
+
+sample_input  =  [[0 for col in range(N_BLOCKS)] for row in range(N_BLOCKS)]
+sample_input[0][0] = 4
+sample_input[2][1] = 2
+sample_input[5][2] = 3
+sample_input[3][3] = 5
+sample_input[6][4] = 6
+sample_input[7][5] = 3
+sample_input[7][6] = 2
+sample_input[1][7] = 4
+sample_input[6][0] = 1
+sample_input[1][1] = 1
+sample_input[2][2] = 1
+sample_input[3][3] = 1
+sample_input[6][4] = 1
+sample_input[7][5] = 1
+sample_input[0][6] = 1
+sample_input[2][7] = 1
+
+sample_input[-8][0] = -4
+sample_input[-2][1] = -2
+sample_input[-5][2] = -3
+sample_input[-3][3] = -5
+sample_input[-6][4] = -6
+sample_input[-7][5] = -3
+sample_input[-7][6] = -2
+sample_input[-1][7] = -4
+sample_input[-6][0] = -1
+sample_input[-1][1] = -1
+sample_input[-2][2] = -1
+sample_input[-3][3] = -1
+sample_input[-6][4] = -1
+sample_input[-7][5] = -1
+sample_input[-8][6] = -1
+sample_input[-2][7] = -1
+
+board = place_icons(board, sample_input)
+
+cv2.imwrite("generated_img.png",board)
---
+++ token_factory/scripts/interact.py
@@ -0,0 +1,77 @@
+from dotenv.main import dotenv_values
+from brownie import Chess_Byte, accounts
+# from brownie.network import priority_fee
+from eth_account.messages import encode_defunct
+from web3 import Web3
+from web3.auto import w3
+
+
+
+env_config = dotenv_values("../.env")
+
+def to_32byte_hex(val):
+   return Web3.toHex(Web3.toBytes(val).rjust(32, b'\0'))
+
+
+def hsign (msg, sk):
+    # Web3.solidityKeccak(types, params).hex()
+    msg = encode_defunct(text=msg)
+    signed_message = w3.eth.account.sign_message(msg, private_key=sk)
+    ec_recover_args = (msghash, v, r, s) = (
+      Web3.toHex(signed_message.messageHash),
+      signed_message.v,
+      to_32byte_hex(signed_message.r),
+      to_32byte_hex(signed_message.s),
+    )
+    return ec_recover_args
+
+def main():
+    # This part is for test in the real program the smart
+    # contract would alredy be deployed
+    deployer = accounts.add()
+    Contract = Chess_Byte.deploy({
+        'from':deployer
+    })
+    
+    token_id = 1
+    tokenURI = """
+        {
+        "attributes" : [
+        {
+            "trait_type": "id",
+            "value":1
+        },
+        {
+            "trait_type": "group_id",
+            "value":1
+        },
+        {
+        "trait_type" : "Year",
+        "value" : "1890"
+        }, {
+        "trait_type" : "Winner",
+        "value" : "Mocha"
+        } ],
+        "description" : "The world's most adorable and sensitive game.",
+        "image" : "https://gateway.pinata.cloud/ipfs/Qmck4myhw8wMnXdJ3E7LVWQqabACUxh
+    FEPxjiaSWXoe1t4",
+        "name" : "The queen gambit"
+        }
+    """
+    Buyer = accounts[0]
+
+    msghash, v, r, s = hsign(str(token_id) + tokenURI + Buyer.address + Contract.address, deployer.private_key)
+    print("Buyer balance before transacting ", Buyer.balance())
+    tx = Chess_Byte[0].buy_token(tokenURI,msghash, v, r, s, token_id, Contract.address,{'from':accounts[0], 'value': Chess_Byte[0].cprice()})
+    print("Buyer balance after", Buyer.balance())
+    Chess_Byte[0].withdraw({'from':deployer})
+    print("Deployer balance after withdrawing", deployer.balance())
+    # print(tx.info, tx.events)
+    print("Token id", Chess_Byte[0].token_size({'from':deployer}))
+    
+
+
+
+
+
+
