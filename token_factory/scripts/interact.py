707ab87c5ec5c60de6812a54fbca89a814cd688e
hileamlakb <ehmyitayew@college.harvard.edu>
2022-01-03 07:25:08 -0500

    Decrease contract transaction costs by storing only storing URI on the blockchain

--- token_factory/scripts/interact.py
+++ token_factory/scripts/interact.py
@@ -1,24 +1,27 @@
 from dotenv.main import dotenv_values
 from brownie import Chess_Byte, accounts
-# from brownie.network import priority_fee
-from eth_account.messages import encode_intended_validator, encode_defunct
+from eth_account.messages import encode_defunct
 from web3 import Web3
 from web3.auto import w3
-from eth_abi import encode_single, encode_abi
-
-
 
 env_config = dotenv_values("../.env")
+
 
 def to_32byte_hex(val):
    return Web3.toHex(Web3.toBytes(val).rjust(32, b'\0'))
 
-
-def hsign (params, values, validator, sk):
-    # Web3.solidityKeccak(types, params).hex()
-    # msg = encode_intended_validator(validator, text=msg)
+def hsign (params, values, sk):
+    """
+        signs a hashed version of the values inside values
+        The hashing algorithm is equivalent to solidity 0.7's
+        Keccak256(abi.encodePacked())
+        
+        params - a list of the types of the equivlanet values inside values
+        values - values to be merged hashed and signed
+        sk - private key to signe them with
+    """
     msg = encode_defunct(primitive=Web3.solidityKeccak(params,values))
-    print(msg)
+    # print(msg)
     signed_message = w3.eth.account.sign_message(msg, private_key=sk)
     ec_recover_args = (msghash, v, r, s) = (
       Web3.toHex(signed_message.messageHash),
@@ -37,42 +40,26 @@ def main():
     })
     
     token_id = 1
-    tokenURI = """
+    token = """
         {
-        "attributes" : [
-        {
-            "trait_type": "id",
-            "value":1
-        },
-        {
-            "trait_type": "group_id",
-            "value":1
-        },
-        {
-        "trait_type" : "Year",
-        "value" : "1890"
-        }, {
-        "trait_type" : "Winner",
-        "value" : "Mocha"
-        } ],
-        "description" : "The world's most adorable and sensitive game.",
-        "image" : "https://gateway.pinata.cloud/ipfs/Qmck4myhw8wMnXdJ3E7LVWQqabACUxh
-    FEPxjiaSWXoe1t4",
         "name" : "The queen gambit"
+        "id" :1,
+        "group_id": 1,
+        "time": "1890",
+        "player1": "Winner",
+        "player3": "Winner",
+        "winner" : "Mocha"
+        "description" : "The world's most adorable and sensitive game.",
+        "image" : "https://gateway.pinata.cloud/ipfs/Qmck4myhw8wMnXdJ3E7LVWQqabACUxhFEPxjiaSWXoe1t4",
         }
     """
+    tokenURI = "https://gateway.pinata.cloud/ipfs/Qmck4myhw8wMnXdJ3E7LVWQqabACUxhFEPxjiaSWXoe1t4"
     Buyer = accounts[0]
 
-    # msghash, v, r, s = hsign(str(token_id) + tokenURI + Buyer.address + Contract.address, deployer.address, deployer.private_key)
     # Buyer address is to make sure on buyer won't buy using other people's hash
-    msghash, v, r, s = hsign(['uint256', 'string', 'address', 'address'],[token_id, tokenURI, Buyer.address, Contract.address], deployer.address, deployer.private_key)
-    # print("Buyer balance before transacting ", Buyer.balance())
-    tx = Chess_Byte[0].buy_token(token_id,tokenURI,msghash, v, r, s,{'from':accounts[0], 'value': Chess_Byte[0].cprice()})
-    # print("Buyer balance after", Buyer.balance())
+    msghash, v, r, s = hsign(['uint256', 'string', 'string', 'address', 'address'],[token_id, token, tokenURI, Buyer.address, Contract.address], deployer.private_key)
+    tx = Chess_Byte[0].buy_token(token_id,token, tokenURI,msghash, v, r, s,{'from':accounts[0], 'value': Chess_Byte[0].cprice()})
     Chess_Byte[0].withdraw({'from':deployer})
-    # print("Deployer balance after withdrawing", deployer.balance())
-    # print(tx.info, tx.events)
-    # print("Token id", Chess_Byte[0].token_size({'from':deployer}))
     print(tx.events)
 
 
